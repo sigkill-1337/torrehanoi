8583e421d2eb48a2df9088ffece6dc8deef8320b
initramfs <108516418+sigkill-1337@users.noreply.github.com>
2026-09-01 18:57:22 -0500

    Translate resolverHanoi method to Spanish

--- src/App.java
+++ src/App.java
@@ -1,15 +1,23 @@
 public class App {
-    public static void resolverHanoi(int n, char source, char target, char auxiliary) {
+
+    public static void resolverHanoi(int n, char origen, char destino, char auxiliar) {
+
         if (n == 1) {
-            System.out.println("Mover disco 1 de " + source + " a " + target);
+            System.out.println("Mover disco 1 de " + origen + " a " + destino);
             return;
         }
-        resolverHanoi(n - 1, source, auxiliary, target);
-        System.out.println("Mover disco " + n + " de " + source + " a " + target);
-        resolverHanoi(n - 1, auxiliary, target, source);
+
+        resolverHanoi(n - 1, origen, auxiliar, destino);
+
+        System.out.println("Mover disco " + n + " de " + origen + " a " + destino);
+
+        resolverHanoi(n - 1, auxiliar, destino, origen);
     }
+
     public static void main(String[] args) throws Exception {
-        int n = 3; // Number of disks
-        resolverHanoi(n, 'A', 'C', 'B'); // A, B, and C are the names of the rods
+
+        int n = 3; // Número de discos
+
+        resolverHanoi(n, 'A', 'C', 'B');
     }
 }
